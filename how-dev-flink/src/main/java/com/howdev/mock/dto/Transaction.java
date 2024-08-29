04998e53f50fcfac333a31b5bc6e5e4ad64874bd
zhifenghao123 <zhifenghao123@outlook.com>
2024-08-29 12:58:19 +0800

    修改交易数据mock逻辑

--- how-dev-flink/src/main/java/com/howdev/mock/dto/Transaction.java
+++ how-dev-flink/src/main/java/com/howdev/mock/dto/Transaction.java
@@ -3,13 +3,31 @@ package com.howdev.mock.dto;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.*;
 
 @Data
 public class Transaction {
-    public static final long[] USER_ID_RANGE = {0L, 99999L};
-    public static final double[] AMOUNT_RANGE = {-1000000.0, 1000000.0};
-    public static final String[] TYPE_ENUM = {"餐饮", "购物", "娱乐", "交通", "学习", "住房", "其他"};
+    public static final long[] USER_ID_RANGE = {1000L, 9999L};
+    public static final double INIT_BALANCE = 100000.0;
+    public static final String[] EXPENSES_TYPE_ENUM = {"娱乐", "交通", "学习", "网购", "向他人转账", "为其他App充值"};
+    public static final String[] INCOME_TYPE_ENUM = {"工资", "兼职", "理财生息", "他人转账", "其他App提现"};
+
+    public static final Map<String, String> EXPENSES_TYPE_TO_AMOUNT_RANGE = Collections.unmodifiableMap(new HashMap<String, String>() {{
+        put("娱乐", "1-200");
+        put("交通", "1-300");
+        put("学习", "10-1000");
+        put("网购", "1-200");
+        put("向他人转账", "1-100");
+        put("为其他App充值", "1-100");
+    }});
+    public static final Map<String, String> INCOME_TYPE_TO_AMOUNT_RANGE = Collections.unmodifiableMap(new HashMap<String, String>() {{
+        put("工资", "15000-20000");
+        put("兼职", "500-1000");
+        put("理财生息", "100-500");
+        put("他人转账", "100-1000");
+        put("其他App提现", "20-200");
+
+    }});
     public static final String[] LOCATION_ENUM= {"北京市", "上海市", "重庆市", "天津市", "广东省广州市", "广东省深圳市",
             "江苏省南京市", "江苏省苏州市", "浙江省杭州市", "浙江省宁波市", "安徽省合肥市", "山东省济南市", "山东省青岛市",
             "河南省郑州市", "河南省洛阳市", "湖北省武汉市", "湖南省长沙市", "四川省成都市", "福建省福州市", "江西省南昌市", "陕西省西安市", "陕西省宝鸡市","辽宁省沈阳市"};
@@ -30,7 +48,7 @@ public class Transaction {
      * 交易金额，单位：元
      * 正数代表收入，负数代表支出
      */
-    private double amount;
+    private String amount;
     /**
      * 交易类别
      */
@@ -43,5 +61,9 @@ public class Transaction {
      * 交易发生时间
      */
     private Date occurredTime;
+    /**
+     * 交易发生后账户余额
+     */
+    private String balance;
 
 }

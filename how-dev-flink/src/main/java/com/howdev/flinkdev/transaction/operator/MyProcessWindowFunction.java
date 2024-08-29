8385029fec32624228ddeeacded26716235ebdb5
zhifenghao123 <zhifenghao123@outlook.com>
2024-08-30 00:06:21 +0800

    新增自定义operator

---
+++ how-dev-flink/src/main/java/com/howdev/flinkdev/transaction/operator/MyProcessWindowFunction.java
@@ -0,0 +1,29 @@
+package com.howdev.flinkdev.transaction.operator;
+
+import com.howdev.mock.dto.TransactionAggregateResult;
+import com.howdev.mock.util.TimeUtil;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+public class MyProcessWindowFunction extends ProcessWindowFunction<TransactionAggregateResult, TransactionAggregateResult, Tuple2<Long, String>, TimeWindow> {
+    @Override
+    public void process(Tuple2<Long, String> keyTuple, ProcessWindowFunction<TransactionAggregateResult, TransactionAggregateResult, Tuple2<Long, String>, TimeWindow>.Context context, Iterable<TransactionAggregateResult> iterable, Collector<TransactionAggregateResult> collector) throws Exception {
+        long windowStartTime = context.window().getStart();
+        long windowEndTime = context.window().getEnd();
+
+        String formatWindowStartMinuteTimeText = TimeUtil.formatTimeStamp(windowStartTime, "yyyy-MM-dd HH:mm:00");
+
+        Long userId = keyTuple.f0;
+        String occurredLocation = keyTuple.f1;
+
+        TransactionAggregateResult transactionAggregateResult = iterable.iterator().next();
+        transactionAggregateResult.setOccurredMinuteTimeStamp(windowEndTime);
+        transactionAggregateResult.setOccurredMinuteTimeText(formatWindowStartMinuteTimeText);
+        transactionAggregateResult.setUserId(userId);
+        transactionAggregateResult.setOccurredLocation(occurredLocation);
+
+        collector.collect(transactionAggregateResult);
+    }
+}

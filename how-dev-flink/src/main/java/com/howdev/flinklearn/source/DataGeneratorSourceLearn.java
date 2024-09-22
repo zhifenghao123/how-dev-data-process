fa73695df59f617380e7dd6f97fb66db4c60ef54
zhifenghao123 <zhifenghao123@outlook.com>
2024-09-22 14:42:17 +0800

    Flink DataStreamSource

---
+++ how-dev-flink/src/main/java/com/howdev/flinklearn/source/DataGeneratorSourceLearn.java
@@ -0,0 +1,42 @@
+package com.howdev.flinklearn.source;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
+import org.apache.flink.connector.datagen.source.DataGeneratorSource;
+import org.apache.flink.connector.datagen.source.GeneratorFunction;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class DataGeneratorSourceLearn {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.setParallelism(3);
+
+        DataGeneratorSource<String> dataGeneratorSource = new DataGeneratorSource<>(
+                // GeneratorFunction接口，输入类型固定式Long
+                new GeneratorFunction<Long, String>() {
+                    @Override
+                    public String map(Long value) throws Exception {
+                        return "Number: " + value;
+                    }
+                },
+                // 自动生成的数字序列的最大值（从0自增，到达该最大值的前一个数），到达这个值就停止了
+                100L,
+                // 限速策略，每秒生成1个
+                RateLimiterStrategy.perSecond(1),
+                Types.STRING
+        );
+
+
+        DataStreamSource<String> dataStreamSource = env.fromSource(dataGeneratorSource, WatermarkStrategy.noWatermarks(), "DataGeneratorSource");
+
+        dataStreamSource.print();
+
+        env.execute();
+    }
+}
